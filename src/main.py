6c16eb4a2a963809efa689cad00e3366f38d905e
Alexandr Kovalchuk <shelter.play.akk@mail.ru>
2025-06-02 22:11:27 +0700

    modified:   .gitignore 	new file:   Dockerfile 	new file:   __init__.py 	new file:   src/__init__.py 	modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from src.users.router import router as user_router
 from src.data.router import router as data_router
 
 app = FastAPI(title="IP_belkin")
 PORT=8080
+HOST = "127.0.0.1"
 
 app.include_router(user_router)
 app.include_router(data_router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 if __name__ == "__main__":
-    uvicorn.run('src.main:app', host = '127.0.0.1', port=PORT, reload = True)
+    uvicorn.run('src.main:app', host = HOST, port=PORT, reload = True)
